01d11bf8a3f28a0d3d5340ac974386681f99220f
Anthony W. Weed <devnoot@gmail.com>
2024-05-13 21:46:20 -0400

    Update vite.config.ts
    Signed-off-by: Anthony W. Weed <devnoot@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
+      '/api/': {
         target: 'https://www.doomworld.com/idgames/api/api.php',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
